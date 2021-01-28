b76caf82a33367bedabb62f58aa5b4257d67cd03
Bertrand <dan.bertrand.hs@gmail.com>
2021-01-28 18:11:35 +0100

    Still on page details, saving before the drama

--- src/js/PageDetail.js
+++ src/js/PageDetail.js
@@ -12,9 +12,13 @@ const PageDetail = (argument) => {
         .then((response) => response.json())
         .then((response) => {
 
- 
-          console.log(response)
-          let { name, released, description, background_image, developers, genres, tags, clip, rating, website, metacritic_platforms} = response;
+
+          console.log("*****************************")
+          console.log(response.ratings_count)
+          console.log("*****************************")
+
+
+          let { name, released, description, background_image, developers, genres, tags, clip, rating, ratings_count, website, metacritic_platforms, stores, id} = response;
 
           let articleDOM = document.querySelector(".page-detail .article");
 
@@ -23,10 +27,12 @@ const PageDetail = (argument) => {
           articleDOM.querySelector("p.release-date span").innerHTML = released;
           articleDOM.querySelector("p.description").innerHTML = description;
           articleDOM.querySelector("p.developers").innerHTML = developers[0].name;
-          articleDOM.querySelector("p.rating").innerHTML = rating;
-          document.querySelector("#detail-info > a").innerHTML = response.website ;
-          document.querySelector("#detail-info > a").href = response.website
-          document.querySelector("#detail-trailer > video").src = clip.clip
+          articleDOM.querySelector("p.rating").innerHTML = `${rating}/5 - ${ratings_count} votes`;
+          articleDOM.querySelector("#detail-info > a").innerHTML = website ;
+          articleDOM.querySelector("#detail-info > a").href = website
+          articleDOM.querySelector("#detail-trailer > video").src = clip.clip
+          articleDOM.querySelector("#detail-youtube").innerHTML += `<iframe id="detailvideo" width="420" height="315" src = https://www.youtube.com/embed/${clip.video}></iframe>`
+
           
           genres.forEach((genre) => {
             articleDOM.querySelector("p.genres").innerHTML += genre.name + "/";
@@ -36,14 +42,14 @@ const PageDetail = (argument) => {
             articleDOM.querySelector("p.tags").innerHTML += tag.name + "/";
           })
 
-          response.stores.forEach((store) => {
+          stores.forEach((store) => {
                let newLink = document.createElement("a"); 
                newLink.innerHTML = store.store.name
                newLink.href = store.url
                document.querySelector("#detail-buy").appendChild(newLink)
           })
 
-          fetch(`https://api.rawg.io/api/games/${response.id}/screenshots?&page_size=9`)
+          fetch(`https://api.rawg.io/api/games/${id}/screenshots?&page_size=6`)
             .then((response) => response.json())
             .then((response) => {
           response.results.forEach((screenshot) => {
@@ -51,46 +57,25 @@ const PageDetail = (argument) => {
           newScreen.src = screenshot.image
           document.querySelector("#detail-screenshots").appendChild(newScreen)
           })
-            });
+          });
 
 
-          fetch(`https://api.rawg.io/api/games/${response.id}/suggested?&page_size=9`)
-            .then((response) => response.json())
-            .then((response) => {
-          response.results.forEach((game) => {
 
-            console.log("******************************")
-            console.log(game)
-            console.log("******************************")
 
-            document.querySelector("#detail-similar-games").innerHTML += `
-                  
-                  <div class="cardGame">
 
-                    <div class="content">
-                    <a href="#pagedetail/${game.id}"></a>
-                      <div class="image-list">
-                        <img  src="${game.background_image}">
-                      </div>
 
-                      <div class="bottom-content">
-                      <h1>${game.name}</h1>
 
-                 
-                     
-                     </div>
-                    </div>
 
-                    <div class="more">
-                          <h2>${game.released}</h2>
-                          <h3>${rating.join("")}</h3>
-                          <h3>${category}</h3>
-                    </div>
-                  </div>
-                `;
 
-          })
-            });
+
+
+          // fetch(`https://api.rawg.io/api/games/${response.id}/suggested?&page_size=9`)
+          //   .then((response) => response.json())
+          //   .then((response) => {
+          // response.results.forEach((game) => {
+
+          // })
+          //   });
 
 
           metacritic_platforms.forEach((platform) => {
@@ -113,15 +98,18 @@ const PageDetail = (argument) => {
         </div>
 
         <div id="detail-info">
-          <h1 class="title"></h1>
+          <div class="header">
+            <h1 class="title"></h1>
+            <p class="rating"></p>
+          </div>
           <p class="release-date">Release date : <span></span></p>
           <p class="description"></p>
           <p class="developers"></p>
           <p class="genres"></p>
-          <p class="rating"></p>
+          
           <p class="platform"></p>
           <p class="tags"></p>
-          <a href="" class="website"></a>
+            <a href="" class="website"></a>
         </div>
         
         <div id="detail-buy">
@@ -144,7 +132,7 @@ const PageDetail = (argument) => {
 
         <div >
           <h2>SIMILAR GAMES</h2>
-          <div id="detail-similar-games" ></div>
+          <div ></div>
         </div>
 
           <p class="clip"></p>
